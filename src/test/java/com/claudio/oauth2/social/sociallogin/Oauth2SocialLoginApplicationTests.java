53c70cbe118b81afc0df928a4ef39fa6ebbcd2df
Claudio Cifuentes <cacifuentesalonso@gmail.com>
2021-06-25 11:31:12 -0500

    Application code

---
+++ src/test/java/com/claudio/oauth2/social/sociallogin/Oauth2SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudio.oauth2.social.sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
